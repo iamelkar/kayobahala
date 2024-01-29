9cdee2c4320eaf21e5d6993df994968161d03d5a
Kharhyll Joy  Laranio <kjlaranio@addunet.ph>
2024-01-29 19:22:10 +0800

    added node modules

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+
+const port = 8000
+
+app.listen(port, () => {
+    console.log(`Server connected to port ${port}`)
+})  
+
+app.get("/", (req, res) => {
+    res.send(`
+        <div>
+        <h1>Hello World!</h1>
+        </div>
+    `)
+    console.log("request received")
+})
